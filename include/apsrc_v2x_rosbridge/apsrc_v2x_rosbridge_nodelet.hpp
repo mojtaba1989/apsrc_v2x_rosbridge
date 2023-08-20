d504ed5ef873a510549be47b2f1d7f28f7d1775d
Mojtaba Bahramgiri <mbahramg@mtu.edu>
2023-08-20 13:59:41 -0400

    init

--- include/apsrc_v2x_rosbridge/apsrc_v2x_rosbridge_nodelet.hpp
+++ include/apsrc_v2x_rosbridge/apsrc_v2x_rosbridge_nodelet.hpp
@@ -1,5 +1,5 @@
 #ifndef APSRC_V2X_ROSBRIDGENL_H
-#define APSRC_V2X_ROSBRIDGEnL_H
+#define APSRC_V2X_ROSBRIDGENL_H
 
 #include <ros/ros.h>
 #include <ros/package.h>
@@ -9,11 +9,10 @@
 #include <thread>
 #include <mutex>
 
-#include <autoware_msgs/LaneArray.h>
-#include <autoware_msgs/VehicleStatus.h>
-#include <geometry_msgs/TwistStamped.h>
-#include <tf/transform_datatypes.h>
-#include <std_msgs/Int32.h>
+#include <autoware_msgs/LaneArray.h> // must be removed
+#include "Ieee1609Dot2Data.h"
+#include "MessageFrame.h"
+
 
 namespace apsrc_v2x_rosbridge
 {
@@ -46,6 +45,10 @@ private:
   AS::Network::UDPServer udp_server_;
   std::thread udp_server_thread_;
   bool udp_server_running_ = false;
+  Ieee1609Dot2Data_t *ieee1609_data_ = 0;
+  MessageFrame_t *j2735_data_ = 0;
+  asn_dec_rval_t ieee1609_rval_t_, j2735_rval_t_;
+  size_t size_;
 
   // Params
   std::string server_ip_;
